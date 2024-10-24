9e7089e015bde649500fbdbe1eee1215343113e7
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-10-24 11:05:55 -0600

    Update Training-JS-#18:-Methods-of-String-object--concat()-split()-and-its-good-friend-join().js

--- Training-JS-#18:-Methods-of-String-object--concat()-split()-and-its-good-friend-join().js
+++ Training-JS-#18:-Methods-of-String-object--concat()-split()-and-its-good-friend-join().js
@@ -1,3 +1,6 @@
+// Title: Training JS #18: Methods of String object
+// Link: https://www.codewars.com/kata/57280481e8118511f7000ffa
+
 /*The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
 
 For example:
