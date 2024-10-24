a7a961880f9d52c8872e1f514b954bf01b156b32
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-10-24 10:57:18 -0600

    Update Convert-a-string-to-an-array.js

--- Convert-a-string-to-an-array.js
+++ Convert-a-string-to-an-array.js
@@ -1,3 +1,6 @@
+// Title: Convert a String to An Array
+// Link: https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
 /*Write a function to split a string and convert it into an array of words.
 
 Examples (Input ==> Output):
