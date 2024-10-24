2cb3febc139faabd48cf9bb18dfc8d5007789f50
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-10-24 10:58:08 -0600

    Update Convert-boolean-values-to-strings-'Yes'-or-'No'.js

--- Convert-boolean-values-to-strings-'Yes'-or-'No'.js
+++ Convert-boolean-values-to-strings-'Yes'-or-'No'.js
@@ -1,3 +1,6 @@
+// Title: Convert boolean values to strings 'Yes' or 'No'
+// Link: https://www.codewars.com/kata/53369039d7ab3ac506000467
+
 /*Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.*/
 
 function boolToWord( bool ){
