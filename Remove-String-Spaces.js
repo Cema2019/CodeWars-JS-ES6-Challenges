a5aa8d99d4cf59ab90fb4e763a831570dad5b17c
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-10-24 11:02:36 -0600

    Update Remove-String-Spaces.js

--- Remove-String-Spaces.js
+++ Remove-String-Spaces.js
@@ -1,3 +1,6 @@
+// Title: Remove String Spaces
+// Link: https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+
 /*Write a function that removes the spaces from the string, then return the resultant string.
 
 Examples:
